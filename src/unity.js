5c4f2b414063d8bc38caaf701d5ce9bea91a6821
js6pak <kubastaron@hotmail.com>
2022-08-29 14:31:48 +0000

    Update unity.js

--- src/unity.js
+++ src/unity.js
@@ -19,6 +19,7 @@ async function createManualActivationFile(unityPath) {
 async function activateManualLicense(unityPath, licenseData) {
     fs.writeFileSync('license.ulf', licenseData);
     const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -manualLicenseFile license.ulf`);
+    fs.unlinkSync('license.ulf');
     if (!stdout.includes('Next license update check is after') && !stdout.includes('License file successfully loaded')) {
         throw new Error('Activation failed');
     }
